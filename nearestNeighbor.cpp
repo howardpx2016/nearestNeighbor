b0ad74bfae24288ec62d3cfeb86d20bba64e2bca
root <root@DESKTOP-10J9EA0.localdomain>
2021-03-20 04:25:39 +0800

    finished forward search, tested on small data set

--- nearestNeighbor.cpp
+++ nearestNeighbor.cpp
@@ -7,10 +7,22 @@
 #include <cmath>
 using namespace std;
 
+void printFeatureSet(vector<int> v) {
+    cout << "{";
+    for (int i = 0; i < v.size(); i++) {
+        if (i+1 < v.size()) {
+            cout << v.at(i) << ", ";
+        }
+        else {
+            cout << v.at(i);
+        }
+    }
+    cout << "}";
+}
+
 double findDistance(vector<double> object_to_classify, vector<double> neighbor_to_classify, vector<int> features_plus_k) {
     double sum = 0;
     double distance = 0;
-    
     //calculate sum
     for (int i = 1; i < object_to_classify.size(); i++) {
         // account for feature i if i is found in features_plus_k
@@ -30,13 +42,9 @@ double leave_one_out_cross_validation(vector<vector<double>> data, vector<int> s
     // create new set to include k
     vector<int> features_plus_k = set_of_features;
     features_plus_k.push_back(k);
-
-    //test: show which features are accounted for
-    cout << "--accounting for feature ";
-    for (int i = 0; i < features_plus_k.size(); i++) {
-        cout << features_plus_k.at(i) << ", ";
-    }
-    cout << endl;
+    // print features_plus_k
+    cout << "       Using features(s) ";
+    printFeatureSet(features_plus_k);
 
     //iterate through each object
     for (int i = 0; i < data.size(); i++) {
@@ -57,7 +65,6 @@ double leave_one_out_cross_validation(vector<vector<double>> data, vector<int> s
                 vector<double> neighbor_to_classify = data.at(n);
 
                 double distance = findDistance(object_to_classify, neighbor_to_classify, features_plus_k); // get distance between object and neighbor
-                
                 //test: show distance of current neighbor
                 // cout << "--Distance between object " << i+1 << " and " << n+1 << " is " << distance << endl;
                 
@@ -69,7 +76,6 @@ double leave_one_out_cross_validation(vector<vector<double>> data, vector<int> s
                 }
             }
         }
-        
         // cout << "--Its nearest neighbor is " << nearest_neighbor << " which is class " << label_neighbor_to_classify << endl;
         // update number of correct answers
         if (label_object_to_classify == label_neighbor_to_classify) {
@@ -85,36 +91,51 @@ double leave_one_out_cross_validation(vector<vector<double>> data, vector<int> s
     } 
     //test: print accuracy
     double accuracy = double(number_correctly_classfied) / double(data.size());
-    cout << "--" << number_correctly_classfied << " correct, " << data.size() - number_correctly_classfied << " wrong. Accuracy = " 
-        << accuracy*100 << "%" << endl;
+    cout << " accuracy is " << accuracy*100 << "%" << endl;
 
     return accuracy;
 }
 
 void featureSearch(vector<vector<double>> data) {
     vector<int> current_set_of_features; // start with an empty set of features
+    vector<int> best_feature_subset; // keep track of most accurate feature set
+    double overall_best_accuracy = 0.0;
+    cout << "Begin Search" << endl;
     
-    for (int i = 1; i < data.size(); i++) {
+    //iterate through each item
+    for (int i = 1; i < data.at(0).size(); i++) {
         cout << "On level " << i << " of the search tree" << endl;
         int feature_to_add_at_this_level = 0; // keep track of feature to add
-        double best_so_far_accuracy = 0.0; // keep track of best accuracy  
-        
-        for (int k = 1; k < data.size(); k++) {
-            // check accuracy of k if k doesn't already exist
+        double level_best_accuracy = 0.0; // keep track of best accuracy 
+
+        // iterate through features
+        for (int k = 1; k < data.at(0).size(); k++) {
+            // check accuracy of k if k doesn't already exist in current featureset
             if (find(current_set_of_features.begin(), current_set_of_features.end(), k) == current_set_of_features.end()) { 
-                cout << "--Considering adding feature " << k << endl;
-                double accuracy = leave_one_out_cross_validation(data, current_set_of_features, k); // get accuracy after adding k to set
+                // get accuracy after adding k to current set
+                double accuracy = leave_one_out_cross_validation(data, current_set_of_features, k); 
                 // update highest accuracy (from feature k) if applicable
-                if (accuracy > best_so_far_accuracy) {
-                    best_so_far_accuracy = accuracy;
+                if (accuracy > level_best_accuracy) {
+                    level_best_accuracy = accuracy;
                     feature_to_add_at_this_level = k;            
                 }      
             }
         }
         
-        current_set_of_features.push_back(feature_to_add_at_this_level);
-        cout << "On level " << i  << ", I added feature " << feature_to_add_at_this_level << " to current set\n" << endl;
+        current_set_of_features.push_back(feature_to_add_at_this_level); // update curr feature set
+        if (level_best_accuracy > overall_best_accuracy) { // update best feature set and accuracy if current set is better
+            overall_best_accuracy = level_best_accuracy;
+            best_feature_subset = current_set_of_features;  
+        }
+
+        // cout << "On level " << i << ", I added feature " << feature_to_add_at_this_level << " to current set, which is now ";
+        cout << "Feature set ";
+        printFeatureSet(current_set_of_features);
+        cout << " was best, accuracy is " << level_best_accuracy*100 << "%" << endl;
     }
+    cout << "\nFinished Search!The best feature subset so is ";
+    printFeatureSet(best_feature_subset);
+    cout << " with an accuracy of " << overall_best_accuracy*100 << "%\n" << endl;
        
 }
 
@@ -122,7 +143,7 @@ int main() {
     // save input data into 2d vector 'data'
     vector<vector<double>> data;
     // ifstream fin("CS170_SMALLtestdata__6.txt");
-    ifstream fin("testIn.txt");
+    ifstream fin("CS170_SMALLtestdata__6.txt");
     if (fin.is_open()) {
         double element;
         while (fin.good()) {
@@ -137,6 +158,7 @@ int main() {
             }
             data.push_back(temp);
         }
+        data.pop_back();
     }
     else {
         cout << "unable to open input file" << endl;
@@ -145,8 +167,9 @@ int main() {
     fin.close();
 
     featureSearch(data);
-    //test accuracy
-    // vector<int> featuresetTest = {};
+    
+    // vector<int> featuresetTest = {3, 6, 9, 12, 15};
+    // printFeatureSet(featuresetTest);
     // double k = leave_one_out_cross_validation(data, featuresetTest, 2);
 
     //test: output data into file
@@ -157,7 +180,7 @@ int main() {
             for (int j = 0; j < data.at(i).size(); j++) {
                 fout << data[i][j] << "  ";
             }
-            if (i < data.size() - 1) {
+            if (i+1 < data.size()) {
                 fout << endl;
             }
         }
